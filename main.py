e881a04a9501747e041a76f47fa6292b7ab835c8
Ubuntu <ubuntu@ip-172-31-0-157.us-west-1.compute.internal>
2018-06-09 19:12:05 +0000

    FEAT: Re train with lower learning rate

--- main.py
+++ main.py
@@ -127,9 +127,9 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
         print("EPOCH {}".format(i))
         for image, label in get_batches_fn(batch_size):
             _, loss = sess.run([train_op, cross_entropy_loss], feed_dict = {input_image: image, correct_label: label,
-                        keep_prob: 0.75, learning_rate: 0.0001})
+                        keep_prob: 0.75, learning_rate: 0.00005})
         print("Loss: {:.3f}\n".format(loss))
-        if loss < .01:
+        if loss < .001:
             print("Low loss, stopping now...")
             return
 tests.test_train_nn(train_nn)
@@ -160,7 +160,7 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
-        epochs = 50
+        epochs = 55
         batch_size = 2
 
         #TF place holders:
